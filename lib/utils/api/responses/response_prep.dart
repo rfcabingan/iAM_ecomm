41e516e2f23c91e9edebed32c5e4d4666616e66d
john-c025 <johntherencecalma@gmail.com>
2026-05-13 14:40:11 +0800

    sharelink

--- lib/utils/api/responses/response_prep.dart
+++ lib/utils/api/responses/response_prep.dart
@@ -615,6 +615,7 @@ class ProductItem {
   final bool isPopular;
   final String imageUrl;
   final String altText;
+  final String shareLink;
 
   ProductItem({
     required this.categoryId,
@@ -631,6 +632,7 @@ class ProductItem {
     required this.isPopular,
     required this.imageUrl,
     required this.altText,
+    required this.shareLink,
   });
 
   static ProductItem? fromJson(dynamic json) {
@@ -651,6 +653,7 @@ class ProductItem {
       isPopular: m['isPopular'] as bool? ?? false,
       imageUrl: m['imageUrl'] as String? ?? '',
       altText: m['altText'] as String? ?? '',
+      shareLink: m['shareLink'] as String? ?? '',
     );
   }
 }
